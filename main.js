c497f791f42337f6013999ed762770e7fc52608a
evshiron <evshiron@gmail.com>
2016-12-03 02:21:57 +0800

    feat(nsisUpdate): initialize

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 
-var { app, BrowserWindow } = require('electron');
-
-var mainWindow = null;
+var { app, dialog, BrowserWindow } = require('electron');
+var { autoUpdater } = require('electron-auto-updater');
 
 require('electron-debug')();
 
-app.on('window-all-closed', function() {
+var mainWindow = null;
 
-	app.quit();
-
-});
-
-app.on('ready', function() {
+function openWindow() {
 
 	mainWindow = new BrowserWindow({
         width: 320,
@@ -31,5 +26,65 @@ app.on('ready', function() {
 		mainWindow = null;
 
 	});
+
+}
+
+function checkUpdate() {
+
+	if(process.platform != 'win32') {
+		return;
+	}
+
+	autoUpdater.on('error', (err) => {
+		console.error(err);
+	});
+
+	autoUpdater.on('checking-for-update', () => {
+		console.log('checking-for-update');
+	});
+
+	autoUpdater.on('update-available', () => {
+		console.log('update-available');
+	});
+
+	autoUpdater.on('update-not-available', () => {
+		console.log('update-not-available');
+	});
+
+	autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName, releaseDate, updateURL) => {
+
+		console.log('update-downloaded');
+
+		dialog.showMessageBox(mainWindow, {
+			message: `New version of ${ releaseName } is available, install now?`,
+			buttons: [ 'Yes', 'No' ],
+			cancelId: 1,
+		}, (response) => {
+
+			if(response != 0) {
+				return;
+			}
+
+			autoUpdater.quitAndInstall();
+
+		});
+
+	});
+
+	autoUpdater.checkForUpdates();
+
+}
+
+app.on('window-all-closed', function() {
+
+	app.quit();
+
+});
+
+app.on('ready', function() {
+
+	openWindow();
+
+	checkUpdate();
 
 });
